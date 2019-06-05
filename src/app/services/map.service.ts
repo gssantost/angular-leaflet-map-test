6c65245d88e29e8b0a86bfcfb4b71b8c2370a08c
Giovanny Santos <gsantos@mbo.cps.local>
2019-06-05 14:01:50 -0400

    add global URL variable

--- src/app/services/map.service.ts
+++ src/app/services/map.service.ts
@@ -2,13 +2,15 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IData } from '../data.interface';
+import { API_URL } from '../global/url';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MapService {
 
-  endpoint: string = 'http://0d814013.ngrok.io/api/v1/markers';
+  //endpoint: string = 'http://0d814013.ngrok.io/api/v1/markers';
+  endpoint: string = API_URL;
 
   constructor(private http: HttpClient) { }
 
